3d677345016d0cd69447cd4889a2d84ebf7ce354
Kenny <kennypluchau@gmail.com>
2024-12-09 18:55:39 -0800

    Initial Commit

---
+++ Project_miss_log/Project_miss_logApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_miss_logApp.swift
+//  Project_miss_log
+//
+//  Created by Kenny Luchau on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_miss_logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
